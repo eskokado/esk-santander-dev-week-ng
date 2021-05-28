51b2e17d69009c94cd883e5c3ce35bb207413206
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-28 12:21:32 -0300

    Create dashboard-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
